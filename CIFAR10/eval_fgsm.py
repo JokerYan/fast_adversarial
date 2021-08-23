1718306e2b508f24cc128f9954755e2c5b1ad9e3
JokerYan <770792058@qq.com>
2021-08-23 16:56:03 +0800

    implement post train

--- CIFAR10/eval_fgsm.py
+++ CIFAR10/eval_fgsm.py
@@ -5,7 +5,7 @@ import os
 import torch
 
 from preact_resnet import PreActResNet18
-from utils import evaluate_pgd, evaluate_standard, get_loaders
+from utils import evaluate_pgd, evaluate_standard, get_loaders, get_train_loaders_by_class, evaluate_pgd_post
 
 pretrained_model_path = os.path.join('.', 'pretrained_models', 'cifar_model_weights_30_epochs.pth')
 logger = logging.getLogger(__name__)
@@ -13,7 +13,6 @@ logger = logging.getLogger(__name__)
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch-size', default=128, type=int)
     parser.add_argument('--data-dir', default='../../cifar-data', type=str)
     return parser.parse_args()
 
@@ -22,17 +21,19 @@ def main():
     args = get_args()
     state_dict = torch.load(pretrained_model_path)
 
-    train_loader, test_loader = get_loaders(args.data_dir, args.batch_size)
+    train_loader, test_loader = get_loaders(args.data_dir, batch_size=1)
+    train_loaders_by_class = get_train_loaders_by_class(args.data_dir, batch_size=128)
     model_test = PreActResNet18().cuda()
     model_test.load_state_dict(state_dict)
     model_test.float()
     model_test.eval()
 
-    pgd_loss, pgd_acc = evaluate_pgd(test_loader, model_test, 50, 10)
+    # pgd_loss, pgd_acc = evaluate_pgd(test_loader, model_test, 50, 10)
+    pgd_loss, pgd_acc, pgd_loss_post, pgd_acc_post = evaluate_pgd_post(test_loader, train_loaders_by_class, model_test, 50, 10)
     test_loss, test_acc = evaluate_standard(test_loader, model_test)
 
-    logger.info('Test Loss \t Test Acc \t PGD Loss \t PGD Acc')
-    logger.info('%.4f \t \t %.4f \t %.4f \t %.4f', test_loss, test_acc, pgd_loss, pgd_acc)
+    logger.info('Test Loss \t Test Acc \t PGD Loss \t PGD Acc \t PGD Post Loss \t PGD Post Acc')
+    logger.info('%.4f \t \t %.4f \t %.4f \t %.4f \t %.4f \t \t %.4f', test_loss, test_acc, pgd_loss, pgd_acc, pgd_loss_post, pgd_acc_post)
 
 
 if __name__ == '__main__':
--- CIFAR10/utils.py
+++ CIFAR10/utils.py
@@ -1,10 +1,15 @@
+import copy
+
 import apex.amp as amp
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-from tqdm import tqdm
+from torch.utils.data import Subset
 from torchvision import datasets, transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 import numpy as np
+from tqdm import tqdm
+import torchattacks
 
 cifar10_mean = (0.4914, 0.4822, 0.4465)
 cifar10_std = (0.2471, 0.2435, 0.2616)
@@ -51,6 +56,33 @@ def get_loaders(dir_, batch_size):
         num_workers=2,
     )
     return train_loader, test_loader
+
+
+def get_train_loaders_by_class(dir_, batch_size):
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(cifar10_mean, cifar10_std),
+    ])
+    num_workers = 2
+    train_dataset = datasets.CIFAR10(
+        dir_, train=True, transform=train_transform, download=True)
+    indices_list = [[] for _ in range(10)]
+    for i in range(len(train_dataset)):
+        label = int(train_dataset[i][1])
+        indices_list[label].append(i)
+    dataset_list = [Subset(train_dataset, indices) for indices in indices_list]
+    train_loader_list = [
+        torch.utils.data.DataLoader(
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            pin_memory=True,
+            num_workers=num_workers,
+        ) for dataset in dataset_list
+    ]
+    return train_loader_list
 
 
 def attack_pgd(model, X, y, epsilon, alpha, attack_iters, restarts, opt=None):
@@ -104,6 +136,128 @@ def evaluate_pgd(test_loader, model, attack_iters, restarts):
             n += y.size(0)
             print('Batch {}  avg acc: {}'.format(i, pgd_acc / n))
     return pgd_loss/n, pgd_acc/n
+
+
+def cal_accuracy(outputs, labels):
+    _, predictions = torch.max(outputs, 1)
+    # collect the correct predictions for each class
+    correct = 0
+    total = 0
+    for label, prediction in zip(labels, predictions):
+        if label == prediction:
+            correct += 1
+        total += 1
+    return correct / total
+
+
+def merge_images(train_images, val_images, ratio, device):
+    batch_size = len(train_images)
+    repeated_val_images = val_images.repeat(batch_size, 1, 1, 1)
+    merged_images = ratio * train_images.to(device) + (1 - ratio) * repeated_val_images.to(device)
+    # image[0][channel] = 0.5 * image[0][channel].to(device) + 0.5 * val_images[0][channel].to(device)
+    return merged_images
+
+
+def post_train(model, images, train_loaders_by_class):
+    alpha = (10 / 255) / std
+    epsilon = (8 / 255) / std
+    loss_func = nn.CrossEntropyLoss()
+    device = torch.device('cuda')
+    model = copy.deepcopy(model)
+    model.train()
+    fix_model = copy.deepcopy(model)
+    attack_model = torchattacks.PGD(model, eps=(8/255)/std, alpha=(2/255)/std, steps=20)
+    optimizer = torch.optim.SGD(lr=0.001,
+                                params=model.parameters(),
+                                momentum=0.9,
+                                nesterov=True)
+    # target_bce_loss_func = TargetBCELoss()
+    # target_bl_loss_func = TargetBLLoss()
+    with torch.enable_grad():
+        # find neighbour
+        original_output = fix_model(images)
+        original_class = torch.argmax(original_output).reshape(1)
+        neighbour_images = attack_model(images, original_class)
+        neighbour_output = fix_model(neighbour_images)
+        neighbour_class = torch.argmax(neighbour_output).reshape(1)
+
+        if original_class == neighbour_class:
+            return model, original_class, neighbour_class, None, None
+
+        loss_list = []
+        acc_list = []
+        for _ in range(50):
+            original_data, original_label = next(iter(train_loaders_by_class[original_class]))
+            neighbour_data, neighbour_label = next(iter(train_loaders_by_class[neighbour_class]))
+
+            data = torch.vstack([original_data, neighbour_data]).to(device)
+            data = merge_images(data, images, 0.7, device)
+            label = torch.hstack([original_label, neighbour_label]).to(device)
+            target = torch.hstack([neighbour_label, original_label]).to(device)
+
+            # generate fgsm adv examples
+            delta = (torch.rand_like(data) * 2 - 1) * epsilon  # uniform rand from [-eps, eps]
+            noise_input = data + delta
+            noise_input.requires_grad = True
+            noise_output = model(noise_input)
+            loss = loss_func(noise_output, label)  # loss to be maximized
+            # loss = target_bce_loss_func(noise_output, label, original_class, neighbour_class)  # bce loss to be maximized
+            input_grad = torch.autograd.grad(loss, noise_input)[0]
+            delta = delta + alpha * torch.sign(input_grad)
+            delta.clamp_(-epsilon, epsilon)
+            adv_input = data + delta
+
+            # generate pgd adv example
+            # attack_model.set_mode_targeted_by_function(lambda im, la: target)
+            # adv_input = attack_model(data, label)
+
+            adv_output = model(adv_input.detach())
+            # adv_class = torch.argmax(adv_output)
+            loss_pos = loss_func(adv_output, label)
+            loss_neg = loss_func(adv_output, target)
+            # bce_loss = target_bce_loss_func(adv_output, label, original_class, neighbour_class)
+            # bl_loss = target_bl_loss_func(adv_output, label, original_class, neighbour_class)
+
+            # loss = torch.mean(loss_list)
+            loss = loss_pos
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            defense_acc = cal_accuracy(adv_output, label)
+            loss_list.append(loss)
+            acc_list.append(defense_acc)
+            print('loss: {:.4f}  acc: {:.4f}'.format(loss, defense_acc))
+    return model, original_class, neighbour_class, loss_list, acc_list
+
+
+def evaluate_pgd_post(test_loader, train_loaders_by_class, model, attack_iters, restarts):
+    epsilon = (8 / 255.) / std
+    alpha = (2 / 255.) / std
+    pgd_loss = 0
+    pgd_acc = 0
+    pgd_loss_post = 0
+    pgd_acc_post = 0
+    n = 0
+    model.eval()
+    for i, (X, y) in enumerate(test_loader):
+        X, y = X.cuda(), y.cuda()
+        pgd_delta = attack_pgd(model, X, y, epsilon, alpha, attack_iters, restarts)
+        with torch.no_grad():
+            output = model(X + pgd_delta)
+            loss = F.cross_entropy(output, y)
+            pgd_loss += loss.item() * y.size(0)
+            pgd_acc += (output.max(1)[1] == y).sum().item()
+            n += y.size(0)
+            print('Batch {}  avg acc: {}'.format(i, pgd_acc / n))
+        post_model, _, _, _, _ = post_train(model, X, train_loaders_by_class)
+        with torch.no_grad():
+            output = model(X + pgd_delta)
+            loss = F.cross_entropy(output, y)
+            pgd_loss_post += loss.item() * y.size(0)
+            pgd_acc_post += (output.max(1)[1] == y).sum().item()
+            n += y.size(0)
+            print('Batch {}  avg post acc: {}'.format(i, pgd_acc_post / n))
+    return pgd_loss/n, pgd_acc/n, pgd_loss_post/n, pgd_acc_post/n
 
 
 def evaluate_standard(test_loader, model):
