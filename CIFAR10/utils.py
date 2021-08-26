19e29044852ec94f4577f61e380725a9a4c41cc6
JokerYan <770792058@qq.com>
2021-08-26 22:25:09 +0800

    random neighbour without merge

--- CIFAR10/utils.py
+++ CIFAR10/utils.py
@@ -183,13 +183,13 @@ def post_train(model, images, train_loaders_by_class):
 
         if original_class == neighbour_class:
             print('original class == neighbour class')
-            return model, original_class, neighbour_class, None, None
+            # return model, original_class, neighbour_class, None, None
 
         loss_list = []
         acc_list = []
         for _ in range(50):
             # randomize neighbour
-            # neighbour_class = (original_class + random.randint(1, 9)) % 10
+            neighbour_class = (original_class + random.randint(1, 9)) % 10
 
             original_data, original_label = next(iter(train_loaders_by_class[original_class]))
             neighbour_data, neighbour_label = next(iter(train_loaders_by_class[neighbour_class]))
