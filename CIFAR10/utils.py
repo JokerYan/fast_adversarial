e1ffcc93e394e1373c673384194bf1d133861457
JokerYan <770792058@qq.com>
2021-08-26 17:11:02 +0800

    use random neighbour class for cifar

--- CIFAR10/utils.py
+++ CIFAR10/utils.py
@@ -1,4 +1,5 @@
 import copy
+import random
 
 import apex.amp as amp
 import torch
@@ -187,6 +188,9 @@ def post_train(model, images, train_loaders_by_class):
         loss_list = []
         acc_list = []
         for _ in range(50):
+            # randomize neighbour
+            neighbour_class = (original_class + random.randint(1, 9)) % 10
+
             original_data, original_label = next(iter(train_loaders_by_class[original_class]))
             neighbour_data, neighbour_label = next(iter(train_loaders_by_class[neighbour_class]))
 
