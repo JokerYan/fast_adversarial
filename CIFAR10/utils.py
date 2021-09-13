95ceae8e8919c50b30d4bd133726404551550f51
JokerYan <770792058@qq.com>
2021-09-13 11:05:57 +0800

    use half negative neighbour delta

--- CIFAR10/utils.py
+++ CIFAR10/utils.py
@@ -269,7 +269,7 @@ def post_train(model, images, train_loader, train_loaders_by_class, args):
             # print((torch.randint(0, 2, size=(len(data),)) - 0.5))
             # adv_input = data + (torch.randint(0, 2, size=(len(data),)) - 0.5).to(device) * 2 * neighbour_delta
             # adv_input = data + (torch.randint(0, 2, size=()) - 0.5).to(device) * 2 * neighbour_delta
-            adv_input = data + -1 * neighbour_delta
+            adv_input = data + -0.5 * neighbour_delta
             # directed_delta = torch.vstack([torch.ones_like(original_data).to(device) * neighbour_delta,
             #                                 torch.ones_like(neighbour_data).to(device) * -1 * neighbour_delta])
             # adv_input = data + directed_delta
