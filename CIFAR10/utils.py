3024e46508358702876b570bee0907d602d19fb5
JokerYan <770792058@qq.com>
2021-10-02 17:44:42 +0800

    remove borrowed attack print

--- CIFAR10/utils.py
+++ CIFAR10/utils.py
@@ -408,9 +408,10 @@ def evaluate_pgd_post(test_loader, train_loader, train_loaders_by_class, model, 
         post_model, original_class, neighbour_class, _, _, neighbour_delta = post_train(model, X + pgd_delta, train_loader, train_loaders_by_class, args)
         # print("ref sim: {}".format(cos_sim(torch.rand_like((pgd_delta.view(-1))), torch.rand_like((neighbour_delta.view(-1))))))
         # print("cos sim: {}".format(cos_sim(pgd_delta.view(-1), neighbour_delta.view(-1))))
-        borrowed_attack_output = model(X - neighbour_delta)
-        borrowed_attack_acc += 1 if torch.argmax(borrowed_attack_output) == pgd_output_class else 0
-        print("borrowed attack acc: {:.4f}".format(borrowed_attack_acc / n))
+
+        # borrowed_attack_output = model(X - neighbour_delta)
+        # borrowed_attack_acc += 1 if torch.argmax(borrowed_attack_output) == pgd_output_class else 0
+        # print("borrowed attack acc: {:.4f}".format(borrowed_attack_acc / n))
         # print("label: {} adv: {} borrowed attack: {}".format(int(y), int(pgd_output_class), int(torch.argmax(borrowed_attack_output))))
 
         # evaluate neighbour found
