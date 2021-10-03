bd078c090c172b3b143d4da65139f6f14a037667
JokerYan <770792058@qq.com>
2021-10-03 09:46:13 +0800

    increase lr

--- CIFAR10/utils.py
+++ CIFAR10/utils.py
@@ -241,7 +241,7 @@ def post_train(model, images, train_loader, train_loaders_by_class, args):
     # model.train()
     fix_model = copy.deepcopy(model)
     attack_model = torchattacks.PGD(model, eps=(8/255)/std, alpha=(2/255)/std, steps=20)
-    optimizer = torch.optim.SGD(lr=0.001,
+    optimizer = torch.optim.SGD(lr=0.01,
                                 params=model.parameters(),
                                 momentum=0.9,
                                 nesterov=True)
