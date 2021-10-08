621b2f00b2d66deda753b4ad39bc7ccc747e7aaa
JokerYan <770792058@qq.com>
2021-10-08 23:56:41 +0800

    include argmax in binary search

--- CIFAR10/boundary_attack_utils.py
+++ CIFAR10/boundary_attack_utils.py
@@ -7,7 +7,7 @@ import numpy as np
 def fine_grained_binary_search(model, x0, y0, theta, initial_lbd = 1.0):
     nquery = 0
     lbd = initial_lbd
-    while model(x0 + lbd*theta) == y0:
+    while torch.argmax(model(x0 + lbd*theta)) == y0:
         lbd *= 2.0
         nquery += 1
 
@@ -19,7 +19,7 @@ def fine_grained_binary_search(model, x0, y0, theta, initial_lbd = 1.0):
     lbd_hi_index = 0
     for i, lbd in enumerate(lambdas):
         nquery += 1
-        if model(x0 + lbd*theta) != y0:
+        if torch.argmax(model(x0 + lbd*theta)) != y0:
             lbd_hi = lbd
             lbd_hi_index = i
             break
@@ -29,7 +29,7 @@ def fine_grained_binary_search(model, x0, y0, theta, initial_lbd = 1.0):
     while (lbd_hi - lbd_lo) > 1e-7:
         lbd_mid = (lbd_lo + lbd_hi)/2.0
         nquery += 1
-        if model(x0 + lbd_mid*theta) != y0:
+        if torch.argmax(model(x0 + lbd_mid*theta)) != y0:
             lbd_hi = lbd_mid
         else:
             lbd_lo = lbd_mid
