1cf4fde5d3a0643e7e30de54f43d4347fc571bba
JokerYan <770792058@qq.com>
2021-10-09 00:25:39 +0800

    print cosine similarity

--- CIFAR10/gradient_estimation.py
+++ CIFAR10/gradient_estimation.py
@@ -100,25 +100,30 @@ def main():
         '''
 
         # gradient gt post model
+        all_gradient_list = []
         for j in range(10):
             images.requires_grad = True
             output = post_model(images, post=True)
             loss = loss_func(output, labels)
             all_gradient = torch.autograd.grad(loss, images)[0]
+            all_gradient_list.append(all_gradient)
             print("gt gradient post: {:.8f}".format(float(all_gradient[0][pixel_c][pixel_x][pixel_y])))
             # print("gt gradient post: {:.8f}".format(float(all_gradient[0][pixel_c+1][pixel_x][pixel_y])))
             # print("gt gradient post: {:.8f}".format(float(all_gradient[0][pixel_c+2][pixel_x][pixel_y])))
+        cos_sim = nn.CosineSimilarity(dim=0)
+        angle = cos_sim(all_gradient_list[0], all_gradient_list[1])
+        print("cosine sim: ", angle)
 
-        # gradient gt normal model with noise in output
-        for j in range(10):
-            images.requires_grad = True
-            output = post_model(images, post=False)
-            output_noise = torch.randn_like(output) * 0.03 + 1
-            loss = loss_func(output * output_noise, labels)
-            all_gradient = torch.autograd.grad(loss, images)[0]
-            print("gt gradient: {:.8f}".format(float(all_gradient[0][pixel_c][pixel_x][pixel_y])))
-            # print("gt gradient: {:.8f}".format(float(all_gradient[0][pixel_c+1][pixel_x][pixel_y])))
-            # print("gt gradient: {:.8f}".format(float(all_gradient[0][pixel_c+2][pixel_x][pixel_y])))
+        # # gradient gt normal model with noise in output
+        # for j in range(10):
+        #     images.requires_grad = True
+        #     output = post_model(images, post=False)
+        #     output_noise = torch.randn_like(output) * 0.03 + 1
+        #     loss = loss_func(output * output_noise, labels)
+        #     all_gradient = torch.autograd.grad(loss, images)[0]
+        #     print("gt gradient: {:.8f}".format(float(all_gradient[0][pixel_c][pixel_x][pixel_y])))
+        #     # print("gt gradient: {:.8f}".format(float(all_gradient[0][pixel_c+1][pixel_x][pixel_y])))
+        #     # print("gt gradient: {:.8f}".format(float(all_gradient[0][pixel_c+2][pixel_x][pixel_y])))
 
         # # boundary attack estimate
         # # theta = torch.rand_like(images)
