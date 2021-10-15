34f507ca591b5bb0d5f7758a847cdff260235b12
JokerYan <770792058@qq.com>
2021-10-16 02:03:25 +0800

    use script to record output

--- CIFAR10/bash/exp01.sh
+++ CIFAR10/bash/exp01.sh
@@ -1,4 +1,5 @@
 TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
+script logs/log_exp01_${TIMESTAMP}.txt
 CUDA_VISIBLE_DEVICES=1 python eval_fgsm.py \
   --pt-data ori_neigh \
   --pt-method adv \
@@ -7,5 +8,5 @@ CUDA_VISIBLE_DEVICES=1 python eval_fgsm.py \
   --pt-iter 50 \
   --pt-lr 0.001 \
   --att-iter 20 \
-  --att-restart 1 \
-  | tee logs/log_exp01_${TIMESTAMP}.txt
+  --att-restart 1
+exit
