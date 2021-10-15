e703da643634238cbde50e18b48ca7ee3edbb47a
JokerYan <770792058@qq.com>
2021-10-16 02:04:42 +0800

    direct output

--- CIFAR10/bash/exp01.sh
+++ CIFAR10/bash/exp01.sh
@@ -1,5 +1,4 @@
 TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
-script logs/log_exp01_${TIMESTAMP}.txt
 CUDA_VISIBLE_DEVICES=1 python eval_fgsm.py \
   --pt-data ori_neigh \
   --pt-method adv \
@@ -8,5 +7,5 @@ CUDA_VISIBLE_DEVICES=1 python eval_fgsm.py \
   --pt-iter 50 \
   --pt-lr 0.001 \
   --att-iter 20 \
-  --att-restart 1
-exit
+  --att-restart 1 \
+  > logs/log_exp01_${TIMESTAMP}.txt
