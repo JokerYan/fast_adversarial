1b0e1ac3b39865cc5789c8be80b0783868cb96c0
JokerYan <770792058@qq.com>
2021-10-16 01:53:54 +0800

    remove log output

--- CIFAR10/bash/exp01.sh
+++ CIFAR10/bash/exp01.sh
@@ -7,5 +7,5 @@ CUDA_VISIBLE_DEVICES=1 python eval_fgsm.py \
   --pt-iter 50 \
   --pt-lr 0.001 \
   --att-iter 20 \
-  --att-restart 1 \
-  2>&1 | tee "logs/log_exp01_${TIMESTAMP}.txt"
+  --att-restart 1
+#  2>&1 | tee "logs/log_exp01_${TIMESTAMP}.txt"
