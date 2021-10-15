a7373b1bd4cc9758a0ce00a3572e45fcb8c530aa
JokerYan <770792058@qq.com>
2021-10-16 01:47:42 +0800

    add timestamp to log

--- CIFAR10/bash/exp01.sh
+++ CIFAR10/bash/exp01.sh
@@ -1,3 +1,4 @@
+TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
 CUDA_VISIBLE_DEVICES=1 python eval_fgsm.py \
   --pt-data ori_neigh \
   --pt-method adv \
@@ -7,4 +8,4 @@ CUDA_VISIBLE_DEVICES=1 python eval_fgsm.py \
   --pt-lr 0.001 \
   --att-iter 20 \
   --att-restart 1 \
-  2>&1 | tee logs/log_exp01.txt
+  2>&1 | tee "logs/log_exp01_${TIMESTAMP}.txt"
