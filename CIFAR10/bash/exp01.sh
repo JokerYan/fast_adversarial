ed41b9871513409f0ec7abc84f8ad4e3f7f20a7c
JokerYan <770792058@qq.com>
2021-10-16 01:10:36 +0800

    include exp01.sh

---
+++ CIFAR10/bash/exp01.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=1 python eval_fgsm.py \
+  --pt-data ori_neigh \
+  --pt-method adv \
+  --adv-dir na \
+  --neigh-method untargeted \
+  --pt-iter 50 \
+  --pt-lr 0.001 \
+  --att-iter 20 \
+  --att-restart 1
