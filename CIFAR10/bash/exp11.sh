1cac9418cb26011f71d1a672ee9c660ec9fb0c6c
JokerYan <770792058@qq.com>
2021-10-18 10:34:22 +0800

    add experiment from 9 to 12

---
+++ CIFAR10/bash/exp11.sh
@@ -0,0 +1,11 @@
+TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
+CUDA_VISIBLE_DEVICES=6 python eval_fgsm.py \
+  --pt-data ori_neigh \
+  --pt-method dir_adv \
+  --adv-dir both \
+  --neigh-method untargeted \
+  --pt-iter 75 \
+  --pt-lr 0.001 \
+  --att-iter 20 \
+  --att-restart 1 \
+  --log-file logs/log_exp11_${TIMESTAMP}.txt
