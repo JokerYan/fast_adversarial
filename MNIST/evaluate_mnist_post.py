5816a49b18168acce083baae827662314f5d755f
JokerYan <770792058@qq.com>
2021-11-21 18:44:57 +0800

    remove original logger

--- MNIST/evaluate_mnist_post.py
+++ MNIST/evaluate_mnist_post.py
@@ -15,11 +15,11 @@ from torch.utils.data import DataLoader, Dataset, Subset
 
 from mnist_net import mnist_net
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    format='[%(asctime)s %(filename)s %(name)s %(levelname)s] - %(message)s',
-    datefmt='%Y/%m/%d %H:%M:%S',
-    level=logging.DEBUG)
+# logger = logging.getLogger(__name__)
+# logging.basicConfig(
+#     format='[%(asctime)s %(filename)s %(name)s %(levelname)s] - %(message)s',
+#     datefmt='%Y/%m/%d %H:%M:%S',
+#     level=logging.DEBUG)
 
 
 def clamp(X, lower_limit, upper_limit):
